508000f02f1d772f6e9b1d0c4ff032410ccd41b4
spacekey <fumihiro.fukuda@gmail.com>
2024-07-14 23:52:45 +0900

    プロジェクト ファイルを追加します。

---
+++ WinFormsApp1ContextMenu/SampleMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using SharpShell.Attributes;
+using SharpShell.SharpContextMenu;
+
+namespace WinFormsApp1ContextMenu
+{
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.AllFiles)]
+    public class SampleMenu : SharpContextMenu
+    {
+        protected override bool CanShowMenu() => true;
+
+        protected override ContextMenuStrip CreateMenu()
+        {
+            var menu = new ContextMenuStrip();
+            menu.Items.Add(new ToolStripSeparator());
+            menu.Items.Add(new ToolStripMenuItem("さんぷる"));
+            menu.Items.Add(new ToolStripSeparator());
+            return menu;
+        }
+    }
+}
